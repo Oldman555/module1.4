591cb9043b997c535e69cd50c43e0b78bde45e21
Александр Задорожный <zadorozhniy76@gmail.com>
2024-10-25 18:19:51 +0300

    listwork

---
+++ lists.py
@@ -0,0 +1,20 @@
+# food=["apple",'banane','cockanut',"cherry"]
+# print(food)
+# print("banane" not in food)
+# food[0] = 'duck'
+# print(food)
+# food.append(1917)
+# print(food)
+# food.remove('cherry')
+# print(food)
+# food.extend('strike')
+# print(food)
+# food.extend([ 2015,True])
+# print(food[0:4])
+
+tuple_ = 1,2,3,4
+print(tuple_)
+tuple_2 = (1,2,3,4,)
+tuple_3 = tuple([1,2,3,4])
+print(tuple_2)
+print(tuple_3)
---
+++ main.py
@@ -0,0 +1,14 @@
+# my_strings=input('Введите текст')
+# print(len(my_strings))
+# print(my_strings.upper())
+# print(my_strings.lower())
+# print(my_strings.replace(' ',''))
+# print(my_strings[0])
+# print(my_strings[-1])
+
+
+print(3)
+
+
+
+
