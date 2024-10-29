a3580af63b80fed7deeabc80cad7a214fa961ed7
Александр Задорожный <zadorozhniy76@gmail.com>
2024-10-29 15:29:09 +0300

    for1

--- for.py
+++ for.py
@@ -3,17 +3,13 @@ primes = []
 not_primes = []
 for i in numbers:
     is_prime = True
-
-    if i>1:
-        for j in range(2,i): # берём период от 2 ( 2>1) до индекса i , ограниченный длиной numbers
-            if i%j == 0:   #
-                is_prime = False
-                break
-
-    if is_prime == True:
-        primes.append(i)
-    else:
-        not_primes.append(i)
-
+    if i > 1:
+        for j in range(2,i):
+            if i%j == 0:
+               is_prime = False
+        if is_prime  :
+            primes.append(i)
+        else :
+            not_primes.append(i)
 print(primes)
 print(not_primes)
